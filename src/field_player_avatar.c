128b5198c2eb491dea97fc8ef6df333b63159d59
LOuroboros <lunosouroboros@gmail.com>
2021-07-21 12:34:17 -0300

    Tweaked the checks in PlayerNotOnBikeMoving Also added running speed to dive in the overworld.

--- src/field_player_avatar.c
+++ src/field_player_avatar.c
@@ -695,16 +695,22 @@ static void PlayerNotOnBikeMoving(u8 direction, u16 heldKeys)
         }
     }
 
-    if (gPlayerAvatar.flags & PLAYER_AVATAR_FLAG_SURFING)
+    if (gPlayerAvatar.flags & PLAYER_AVATAR_FLAG_SURFING
+     || gPlayerAvatar.flags & PLAYER_AVATAR_FLAG_UNDERWATER)
     {
-        if (heldKeys & B_BUTTON)
+        if (heldKeys & B_BUTTON && gSaveBlock2Ptr->autoRun == TRUE)
+            PlayerGoSpeed2(direction); // same speed as running
+        else if (heldKeys & B_BUTTON || gSaveBlock2Ptr->autoRun == TRUE)
             PlayerGoSpeed4(direction); // same speed as the Mach Bike
         else
-            PlayerGoSpeed2(direction); // same speed as running
+            PlayerGoSpeed2(direction);
         return;
     }
 
-    if (!(gPlayerAvatar.flags & PLAYER_AVATAR_FLAG_UNDERWATER) && (heldKeys & B_BUTTON || gSaveBlock2Ptr->autoRun) && FlagGet(FLAG_SYS_B_DASH)
+    if (!(gPlayerAvatar.flags & PLAYER_AVATAR_FLAG_SURFING)
+     && !(gPlayerAvatar.flags & PLAYER_AVATAR_FLAG_UNDERWATER)
+     && (heldKeys & B_BUTTON || gSaveBlock2Ptr->autoRun)
+     && FlagGet(FLAG_SYS_B_DASH)
      && IsRunningDisallowed(gObjectEvents[gPlayerAvatar.objectEventId].currentMetatileBehavior) == 0)
     {
         if (heldKeys & B_BUTTON && gSaveBlock2Ptr->autoRun == TRUE)
