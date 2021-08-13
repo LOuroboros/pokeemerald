0031c4b54772bd15a93900b9c4560bea24ef96b0
LOuroboros <lunosouroboros@gmail.com>
2021-08-13 15:31:50 -0300

    Rearranged the BoxPokemon struct Following the lead of Cancer Fairy (aka cfmnephrite or TheChurchOfCage). I'm not sure if I want to get rid of the event legality (formerly MON_DATA_OBEDIENCE), so for now I'll just keep it. With this I gained 104 bytes in SaveBlock1 and 6720 bytes in PokemonStorage. With the current amount of free space the latter has (9732 bytes), I can add 4 boxes (2412*4) to the PokeStorageSys.

--- src/evolution_scene.c
+++ src/evolution_scene.c
@@ -568,11 +568,8 @@ static void CreateShedinja(u16 preEvoSpecies, struct Pokemon* mon)
         SetMonData(&gPlayerParty[gPlayerPartyCount], MON_DATA_NICKNAME, gSpeciesNames[gEvolutionTable[preEvoSpecies][1].targetSpecies]);
         SetMonData(&gPlayerParty[gPlayerPartyCount], MON_DATA_HELD_ITEM, &data);
         SetMonData(&gPlayerParty[gPlayerPartyCount], MON_DATA_MARKINGS, &data);
-        SetMonData(&gPlayerParty[gPlayerPartyCount], MON_DATA_ENCRYPT_SEPARATOR, &data);
 
         for (i = MON_DATA_COOL_RIBBON; i < MON_DATA_COOL_RIBBON + CONTEST_CATEGORIES_COUNT; i++)
-            SetMonData(&gPlayerParty[gPlayerPartyCount], i, &data);
-        for (i = MON_DATA_CHAMPION_RIBBON; i <= MON_DATA_UNUSED_RIBBONS; i++)
             SetMonData(&gPlayerParty[gPlayerPartyCount], i, &data);
 
         SetMonData(&gPlayerParty[gPlayerPartyCount], MON_DATA_STATUS, &data);
