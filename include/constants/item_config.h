a628204f467daeb40e4de511e7be7ca8af130f2a
LOuroboros <lunosouroboros@gmail.com>
2021-10-18 13:51:52 -0300

    Merged item_expansion (18/10/2021)

--- include/constants/item_config.h
+++ include/constants/item_config.h
@@ -1,6 +1,8 @@
 #ifndef GUARD_CONSTANTS_ITEM_CONFIG_H
 #define GUARD_CONSTANTS_ITEM_CONFIG_H
 
+#include "constants/expansion_branches.h"
+
 #ifndef GEN_3
 #define GEN_3 0
 #define GEN_4 1
@@ -15,6 +17,7 @@
 #define I_KEY_FOSSILS           GEN_7   // In Gen4+, all Gen 3 fossils became regular items.
 #define I_KEY_ESCAPE_ROPE       GEN_7   // In Gen8, Escape Rope became a Key Item. Keep in mind, this will make it free to buy in marts.
 #define I_LEGACY_HEALING_ITEMS  GEN_7   // In Gen7+, certain healing items recover less HP than they used to.
+#define I_SITRUS_BERRY_HEAL     GEN_7   // In Gen4+, Sitrus Berry was changed from healing 30 HP to healing 25% of Max HP.
 
 // Ball config
 #define I_LURE_BALL_MODIFIER    GEN_7   // In Gen7+, Lure Ball's catch multiplier is x5 instead of x3.
