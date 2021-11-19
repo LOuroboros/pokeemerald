35aa1f1f520b277ffbb90366b25ca9844160c6de
LOuroboros <lunosouroboros@gmail.com>
2021-11-19 20:19:39 -0300

    2 debug related changes and a special to choose an item from the bag -Replaced the EventScript prefix for all the scripts that I use in the debug menu to "DebugScript" -Added an option to delete a 'mon at will to the debug menu because why not -Added a special that allows the Player to choose an item from the specified pocket of their bag, inspired by Skeli789

--- src/field_specials.c
+++ src/field_specials.c
@@ -67,6 +67,8 @@
 #include "wild_encounter.h"
 #include "fldeff.h"
 #include "pokedex.h"
+#include "item.h"
+#include "item_menu.h"
 
 EWRAM_DATA bool8 gBikeCyclingChallenge = FALSE;
 EWRAM_DATA u8 gBikeCollisions = 0;
@@ -4598,4 +4600,19 @@ bool8 SetCaughtMon(void)
 {
     GetSetPokedexFlag(SpeciesToNationalPokedexNum(VarGet(VAR_TEMP_1)), FLAG_SET_SEEN);
     GetSetPokedexFlag(SpeciesToNationalPokedexNum(VarGet(VAR_TEMP_1)), FLAG_SET_CAUGHT);
+}
+
+void ChooseItemFromBag(void)
+{
+    switch (VarGet(VAR_TEMP_1))
+    {
+    case ITEMS_POCKET:
+    case BALLS_POCKET:
+    case TMHM_POCKET:
+    case BERRIES_POCKET:
+    case KEYITEMS_POCKET:
+        GoToBagMenu(ITEMMENULOCATION_CHOOSE_ITEM, VarGet(VAR_TEMP_1), CB2_ReturnToFieldContinueScript);
+    default:
+        break;
+    }
 }
