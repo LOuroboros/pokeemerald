788e655a2e717cc70fceada15ee7fb6a6afd1aee
LOuroboros <lunosouroboros@gmail.com>
2021-11-22 21:11:33 -0300

    Allow 'mons to use HM moves they can learn without knowing them
    Thanks to Ghoulslash for helping me iron out the function I wrote to check learnsets.
    Thanks to Jasper for their ScrCmd_checkpartyhmlearnable.
    Thanks to Meister_anon for his snippet of code for SetPartyMonFieldSelectionActions which I used as a base.
    
    Misc:
    -Removed unused HM Moves array.

--- src/scrcmd.c
+++ src/scrcmd.c
@@ -283,6 +283,28 @@ bool8 ScrCmd_callstd_if(struct ScriptContext *ctx)
     return FALSE;
 }
 
+bool8 ScrCmd_checkpartyforhmmove(struct ScriptContext *ctx)
+{
+    u8 i;
+    u16 moveId = ScriptReadHalfword(ctx);
+
+    gSpecialVar_Result = PARTY_SIZE;
+    for (i = 0; i < PARTY_SIZE; i++)
+    {
+        u16 species = GetMonData(&gPlayerParty[i], MON_DATA_SPECIES, NULL);
+        if (!species)
+            break;
+        if (!GetMonData(&gPlayerParty[i], MON_DATA_IS_EGG)
+         && CanSpeciesLearnMove(GetMonData(&gPlayerParty[i], MON_DATA_SPECIES), moveId))
+        {
+            gSpecialVar_Result = i;
+            gSpecialVar_0x8004 = species;
+            break;
+        }
+    }
+    return FALSE;
+}
+
 bool8 ScrCmd_killscript(struct ScriptContext *ctx)
 {
     StopScript(ctx);
