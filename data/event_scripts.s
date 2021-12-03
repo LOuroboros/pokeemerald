03ee55b3c55f14c1de279bf4642293632604ab83
LOuroboros <lunosouroboros@gmail.com>
2021-12-02 23:10:07 -0300

    Implemented dynamic fossil revival system Thanks to Revo/ProjectRevoTPP.
    Misc:
    -Heavily reworked the scripts of the 2nd floor of the Devon Corporation building.
    -Freed up FLAG_RECEIVED_REVIVED_FOSSIL_MON as a result of how I reworked the fossil revival script.

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -563,6 +563,7 @@ gStdScripts_End::
 	.include "data/maps/Route119_WeatherInstitute_2F/scripts.inc"
 	.include "data/maps/Route119_House/scripts.inc"
 	.include "data/maps/Route124_DivingTreasureHuntersHouse/scripts.inc"
+	.include "data/maps/Route103North/scripts.inc"
 
 	.include "data/scripts/std_msgbox.inc"
 	.include "data/scripts/trainer_battle.inc"
@@ -1122,5 +1123,4 @@ Common_EventScript_LegendaryFlewAway::
 	.include "data/text/birch_speech.inc"
 	.include "data/scripts/misc_scripts.inc"
 	.include "data/scripts/questionnaire.inc"
-
-	.include "data/maps/Route103North/scripts.inc"
+	.include "data/scripts/fossils.inc"
