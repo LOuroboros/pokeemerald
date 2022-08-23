a2391f3a4e7f35464032a8c06c7a1dcd170a07da
LOuroboros <lunosouroboros@gmail.com>
2022-08-23 11:52:39 -0300

    Revert "Allow to teach pre-evo moves via move relearning" This reverts commit 82ddb903c9cda6569264cf4c637a3432b382d7f7.
    In retrospect, this wasn't a good idea.
    I should have checked the egg move learnsets first. Some of them are too broken to make proper use of this without reworking most of them.

--- src/pokemon.c
+++ src/pokemon.c
@@ -7031,50 +7031,41 @@ u32 CanSpeciesLearnTMHM(u16 species, u8 tm)
     return FALSE;
 }
 
-static void GetMoveRelearnerSpeciesMoves(u16 species, u16 *moves, u16 *knownMoves, u8 *numMoves)
-{
-    int i, j;
-
-    for (i = 1; i < NUM_SPECIES; i++)
-    {
-        for (j = 0; j < EVOS_PER_MON && gEvolutionTable[i][j].targetSpecies != species; j++)
-            ;
-
-        if (j >= EVOS_PER_MON)
-            continue;
-
-        // if this is a preevolution, collect those moves first
-        GetMoveRelearnerSpeciesMoves(i, moves, knownMoves, numMoves);
-        break;
-    }
-
-    for (i = 0; *numMoves < MAX_LEVEL_UP_MOVES && gLevelUpLearnsets[species][i].move != LEVEL_UP_END; i++)
-    {
-        for (j = 0; j < MAX_MON_MOVES && knownMoves[j] != gLevelUpLearnsets[species][i].move; j++)
-            ;
-
-        if (j < MAX_MON_MOVES)
-            continue;
-
-        for (j = 0; j < *numMoves && moves[j] != gLevelUpLearnsets[species][i].move; j++)
-            ;
-
-        if (j == *numMoves)
-            moves[(*numMoves)++] = gLevelUpLearnsets[species][i].move;
-    }
-}
-
 u8 GetMoveRelearnerMoves(struct Pokemon *mon, u16 *moves)
 {
-    u16 knownMoves[MAX_MON_MOVES];
+    u16 learnedMoves[4];
     u8 numMoves = 0;
     u16 species = GetMonData(mon, MON_DATA_SPECIES, 0);
-    int i;
+    u8 level = GetMonData(mon, MON_DATA_LEVEL, 0);
+    int i, j, k;
 
     for (i = 0; i < MAX_MON_MOVES; i++)
-        knownMoves[i] = GetMonData(mon, MON_DATA_MOVE1 + i, 0);
+        learnedMoves[i] = GetMonData(mon, MON_DATA_MOVE1 + i, 0);
 
-    GetMoveRelearnerSpeciesMoves(species, moves, knownMoves, &numMoves);
+    for (i = 0; i < MAX_LEVEL_UP_MOVES; i++)
+    {
+        u16 moveLevel;
+
+        if (gLevelUpLearnsets[species][i].move == LEVEL_UP_END)
+            break;
+
+        moveLevel = gLevelUpLearnsets[species][i].level;
+
+        if (moveLevel <= level)
+        {
+            for (j = 0; j < MAX_MON_MOVES && learnedMoves[j] != gLevelUpLearnsets[species][i].move; j++)
+                ;
+
+            if (j == MAX_MON_MOVES)
+            {
+                for (k = 0; k < numMoves && moves[k] != gLevelUpLearnsets[species][i].move; k++)
+                    ;
+
+                if (k == numMoves)
+                    moves[numMoves++] = gLevelUpLearnsets[species][i].move;
+            }
+        }
+    }
 
     return numMoves;
 }
