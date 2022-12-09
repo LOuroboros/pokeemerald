ba8aa6051483c6501194dd3da0c84c86fce9db79
LOuroboros <lunosouroboros@gmail.com>
2022-12-09 02:51:51 -0300

    Fine-tweaking the new GBS track to build a ROM

--- sound/songs/gbs/rse/gbs_route101.s
+++ sound/songs/gbs/rse/gbs_route101.s
@@ -1,32 +1,32 @@
-; Pokémon R/S/E - Route 101
-; Demixed by Mmmmmm
-; https://pastebin.com/VstjfaGf
-; https://hax.iimarckus.org/topic/6777/
+	.include "asm/macros.inc"
 
-Music_Route101RSE:
-	musicheader 4, 1, Music_Route101RSE_Ch1
-	musicheader 1, 2, Music_Route101RSE_Ch2
-	musicheader 1, 3, Music_Route101RSE_Ch3
-	musicheader 1, 4, Music_Route101RSE_Ch4
+	.section .rodata
+	.global gbs_route101
+	.align 1
 
-Music_Route101RSE_Ch1:
+@ Pokémon R/S/E - Route 101
+@ Demixed by Mmmmmm, ported to GBS by Lunos
+@ https://pastebin.com/VstjfaGf
+@ https://hax.iimarckus.org/topic/6777/
+
+gbs_route101_Ch1:
+	gbs_switch 0
+@ Start
 	tempo 166
-	volume $77
-	tone $0001
-	vibrato $10, $15
-	stereopanning $f
-	notetype $c, $83
-	note __, 2
-Music_Route101RSE_Ch1_loop:
-	intensity $84
-	dutycycle $3
-	callchannel Music_Route101RSE_Ch1_branch_1
-	callchannel Music_Route101RSE_Ch1_branch_1
-	intensity $82
-	dutycycle $0
-	callchannel Music_Route101RSE_Ch1_branch_2
-	callchannel Music_Route101RSE_Ch1_branch_3
-	callchannel Music_Route101RSE_Ch1_branch_3
+	volume 7, 7
+	pitch_offset 1
+	vibrato 16, 1, 5
+	stereo_panning FALSE, FALSE
+	note_type 12, 8, 3
+	rest 2
+gbs_route101_Ch1_loop:
+	volume_envelope 8, 4
+	sound_call gbs_route101_Ch1_branch_1
+	volume_envelope 8, 2
+	duty_cycle 0
+	sound_call gbs_route101_Ch1_branch_2
+	sound_call gbs_route101_Ch1_branch_3
+	sound_call gbs_route101_Ch1_branch_3
 	octave 5
 	note E_, 1
 	octave 4
@@ -51,108 +51,107 @@ Music_Route101RSE_Ch1_loop:
 	note E_, 1
 	note A_, 1
 	note E_, 1
-	note D#, 1
-	note __, 1
+	note Ds, 1
+	rest 1
 	note D_, 1
-	note C#, 2
+	note Cs, 2
 	octave 4
 	note A_, 2
-	callchannel Music_Route101RSE_Ch1_branch_2
-	intensity $83
-	dutycycle $3
+	sound_call gbs_route101_Ch1_branch_2
+	volume_envelope 8, 3
+	duty_cycle 3
 	octave 3
-	note A#, 1
+	note As, 1
 	note D_, 1
 	note F_, 1
-	note A#, 1
+	note As, 1
 	octave 4
 	note D_, 4
 	octave 5
-	intensity $84
-	dutycycle $2
+	volume_envelope 8, 4
+	duty_cycle 2
 	note F_, 2
-	intensity $64
+	volume_envelope 6, 4
 	note F_, 2
-	intensity $84
-	note A#, 2
-	intensity $64
-	note A#, 2
-	intensity $83
-	dutycycle $3
+	volume_envelope 8, 4
+	note As, 2
+	volume_envelope 6, 4
+	note As, 2
+	volume_envelope 8, 3
+	duty_cycle 3
 	octave 3
 	note A_, 1
-	note C#, 1
+	note Cs, 1
 	note E_, 1
 	note G_, 1
 	note A_, 4
 	octave 5
-	dutycycle $2
-	intensity $84
+	duty_cycle 2
+	volume_envelope 8, 4
 	note E_, 2
-	intensity $64
+	volume_envelope 6, 4
 	note E_, 2
-	intensity $84
+	volume_envelope 8, 4
 	note A_, 2
-	intensity $64
+	volume_envelope 6, 4
 	note A_, 2
-	loopchannel 0, Music_Route101RSE_Ch1_loop
+	sound_loop 0, gbs_route101_Ch1_loop
 
-Music_Route101RSE_Ch1_branch_1:
+gbs_route101_Ch1_branch_1:
 	octave 3
 	note D_, 2
 	octave 2
 	note A_, 2
-	note __, 2
-	note F#, 1
+	rest 2
+	note Fs, 1
 	note D_, 1
-	note F#, 2
-	note __, 2
+	note Fs, 2
+	rest 2
 	octave 3
 	note D_, 4
 	note E_, 2
 	octave 2
 	note B_, 2
-	note __, 2
+	rest 2
 	note G_, 1
 	note E_, 1
 	note G_, 2
-	note __, 2
+	rest 2
 	octave 3
 	note E_, 4
-	note C#, 1
+	note Cs, 1
 	octave 2
 	note B_, 1
 	octave 3
-	note C#, 1
+	note Cs, 1
 	note D_, 1
 	note E_, 1
-	note F#, 1
+	note Fs, 1
 	note G_, 1
 	note E_, 1
 	note A_, 8
 	octave 4
-	note C#, 1
+	note Cs, 1
 	octave 3
 	note B_, 1
 	octave 4
-	note C#, 1
+	note Cs, 1
 	octave 3
 	note B_, 1
 	octave 4
-	note C#, 1
+	note Cs, 1
 	octave 3
-	note __, 2
+	rest 2
 	note A_, 1
 	octave 4
-	note C#, 2
-	note __, 2
+	note Cs, 2
+	rest 2
 	octave 3
 	note E_, 2
 	octave 2
 	note A_, 2
-	endchannel
 
-Music_Route101RSE_Ch1_branch_2:
+gbs_route101_Ch1_branch_2:
 	octave 4
 	note G_, 1
 	octave 3
@@ -182,34 +181,33 @@ Music_Route101RSE_Ch1_branch_2:
 	note D_, 1
 	note G_, 1
 	octave 4
-	note F#, 1
+	note Fs, 1
 	octave 3
-	note F#, 1
+	note Fs, 1
 	octave 4
 	note D_, 1
 	octave 3
 	note A_, 1
 	octave 4
-	note F#, 1
+	note Fs, 1
 	octave 3
-	note F#, 1
+	note Fs, 1
 	octave 4
 	note D_, 1
 	octave 3
 	note A_, 1
-	note F#, 1
+	note Fs, 1
 	octave 4
 	note D_, 1
-	note F#, 1
+	note Fs, 1
 	note A_, 1
 	note D_, 1
 	note A_, 1
 	octave 5
 	note D_, 1
-	note F#, 1
-	endchannel
+	note Fs, 1
 
-Music_Route101RSE_Ch1_branch_3:
+gbs_route101_Ch1_branch_3:
 	octave 4
 	note G_, 1
 	octave 3
@@ -217,340 +215,350 @@ Music_Route101RSE_Ch1_branch_3:
 	note B_, 1
 	octave 4
 	note E_, 1
-	endchannel
 
-Music_Route101RSE_Ch2:
-	dutycycle $2
-	vibrato $12, $36
-	notetype $c, $77
-	note __, 2
-Music_Route101RSE_Ch2_loop:
-	intensity $77
-	callchannel Music_Route101RSE_Ch2_branch_1
-	callchannel Music_Route101RSE_Ch2_branch_1
-	callchannel Music_Route101RSE_Ch2_branch_2
-	intensity $97
+gbs_route101_Ch2:
+	gbs_switch 1
+	duty_cycle 2
+	vibrato 18, 3, 6
+	note_type 12, 7, 7
+	rest 2
+gbs_route101_Ch2_loop:
+	volume_envelope 7, 7
+	sound_call gbs_route101_Ch2_branch_1
+	sound_call gbs_route101_Ch2_branch_1
+	sound_call gbs_route101_Ch2_branch_2
+	volume_envelope 9, 7
 	octave 4
 	note D_, 1
-	note __, 1
+	rest 1
 	note D_, 1
-	note __, 1
+	rest 1
 	note D_, 1
-	note __, 1
+	rest 1
 	note E_, 1
-	note F#, 1
-	intensity $90
+	note Fs, 1
+	volume_envelope 9, 0
 	note G_, 2
-	intensity $97
+	volume_envelope 9, 7
 	note G_, 6
 	note E_, 4
 	note D_, 4
 	note E_, 1
-	note __, 1
+	rest 1
 	note E_, 1
-	note __, 1
+	rest 1
 	note E_, 1
-	note __, 1
+	rest 1
 	note E_, 1
-	note F#, 1
+	note Fs, 1
 	note E_, 6
-	intensity $77
+	volume_envelope 7, 7
 	octave 1
 	note A_, 2
-	callchannel Music_Route101RSE_Ch2_branch_2
-	note F#, 1
-	note __, 1
+	sound_call gbs_route101_Ch2_branch_2
+	note Fs, 1
+	rest 1
 	note A_, 1
-	note __, 1
+	rest 1
 	octave 2
 	note D_, 2
 	octave 1
-	note F#, 2
-	intensity $97
+	note Fs, 2
+	volume_envelope 9, 7
 	octave 4
-	note A#, 6
+	note As, 6
 	note A_, 2
 	note G_, 4
 	note F_, 2
-	note A#, 2
-	intensity $90
+	note As, 2
+	volume_envelope 9, 0
 	note A_, 8
-	intensity $97
+	volume_envelope 9, 7
 	note A_, 8
-	loopchannel 0, Music_Route101RSE_Ch2_loop
+	sound_loop 0, gbs_route101_Ch2_loop
 
-Music_Route101RSE_Ch2_branch_1:
+gbs_route101_Ch2_branch_1:
 	octave 1
 	note D_, 2
-	note __, 5
+	rest 5
 	note A_, 1
 	note D_, 2
-	note __, 4
+	rest 4
 	note A_, 2
 	note E_, 2
-	note __, 1
+	rest 1
 	note E_, 1
 	octave 2
-	note D#, 2
+	note Ds, 2
 	octave 1
 	note B_, 1
 	note B_, 1
 	note E_, 2
-	note __, 4
+	rest 4
 	note E_, 2
 	note A_, 2
-	note __, 5
+	rest 5
 	note E_, 1
 	note A_, 2
-	note __, 4
+	rest 4
 	octave 2
 	note E_, 1
 	note E_, 1
 	octave 1
 	note A_, 2
-	note __, 2
+	rest 2
 	note A_, 1
-	note __, 2
+	rest 2
 	note E_, 1
 	note A_, 2
-	note __, 2
+	rest 2
 	octave 2
 	note E_, 1
-	note __, 1
+	rest 1
 	octave 1
 	note A_, 2
-	endchannel
 
-Music_Route101RSE_Ch2_branch_2:
+gbs_route101_Ch2_branch_2:
 	note G_, 2
-	note __, 2
+	rest 2
 	note G_, 1
-	note __, 1
+	rest 1
 	octave 2
 	note D_, 2
 	octave 1
 	note G_, 2
-	note __, 4
-	intensity $92
+	rest 4
+	volume_envelope 9, 2
 	note G_, 1
 	note G_, 1
-	intensity $97
-	note F#, 2
-	note __, 1
+	volume_envelope 9, 7
+	note Fs, 2
+	rest 1
 	note A_, 1
 	octave 2
-	note C#, 2
+	note Cs, 2
 	octave 1
 	note D_, 1
 	note E_, 1
-	endchannel
 
-Music_Route101RSE_Ch3:
-	stereopanning $f0
-	notetype $c, $13
-	vibrato $15, $24
-Music_Route101RSE_Ch3_loop:
-	callchannel Music_Route101RSE_Ch3_branch_1
-	callchannel Music_Route101RSE_Ch3_branch_1
+gbs_route101_Ch3:
+	gbs_switch 2
+	stereo_panning FALSE, TRUE
+	note_type 12, 1, 3
+	vibrato 21, 2, 4
+gbs_route101_Ch3_loop:
+	sound_call gbs_route101_Ch3_branch_1
+	sound_call gbs_route101_Ch3_branch_1
 	octave 6
-	note C#, 1
+	note Cs, 1
 	note D_, 1
 	octave 5
 	note B_, 1
-	note __, 1
+	rest 1
 	note B_, 1
-	note __, 1
+	rest 1
 	note B_, 1
-	note __, 1
+	rest 1
 	note B_, 1
 	octave 6
-	note C#, 1
+	note Cs, 1
 	note D_, 4
 	octave 5
 	note D_, 4
-	callchannel Music_Route101RSE_Ch3_branch_2
+	sound_call gbs_route101_Ch3_branch_2
 	note G_, 1
-	note __, 1
+	rest 1
 	octave 4
 	note B_, 1
-	note __, 1
+	rest 1
 	octave 5
-	note C#, 1
-	note __, 1
+	note Cs, 1
+	rest 1
 	note E_, 1
-	note F#, 1
+	note Fs, 1
 	note G_, 4
 	note B_, 4
-	callchannel Music_Route101RSE_Ch3_branch_2
+	sound_call gbs_route101_Ch3_branch_2
 	note B_, 1
-	note __, 1
+	rest 1
 	note B_, 1
-	note __, 1
+	rest 1
 	note B_, 1
-	note __, 1
+	rest 1
 	note B_, 1
 	octave 6
-	note C#, 1
+	note Cs, 1
 	note D_, 4
 	octave 5
 	note G_, 4
 	octave 6
-	note F#, 1
-	note __, 1
+	note Fs, 1
+	rest 1
 	note G_, 1
-	note __, 1
-	note F#, 1
-	note __, 1
-	note F#, 1
+	rest 1
+	note Fs, 1
+	rest 1
+	note Fs, 1
 	note E_, 1
 	note D_, 6
-	note __, 2
+	rest 3
 	note D_, 6
 	note C_, 2
 	octave 5
-	note A#, 4
+	note As, 4
 	note F_, 2
 	octave 6
 	note D_, 2
-	note C#, 8
-	intensity $23
-	note C#, 6
-	intensity $13
-	loopchannel 0, Music_Route101RSE_Ch3_loop
+	note Cs, 8
+	volume_envelope 2, 3
+	note Cs, 6
+	volume_envelope 1, 3
+	sound_loop 0, gbs_route101_Ch3_loop
 
-Music_Route101RSE_Ch3_branch_1:
+gbs_route101_Ch3_branch_1:
 	octave 5
 	note A_, 1
 	note B_, 1
 	note A_, 1
-	note __, 1
+	rest 1
 	note A_, 1
-	note __, 1
+	rest 1
 	note G_, 1
-	note __, 1
-	note F#, 2
-	note __, 2
-	note F#, 1
-	note __, 1
+	rest 1
+	note Fs, 2
+	rest 2
+	note Fs, 1
+	rest 1
 	note G_, 1
-	note __, 1
+	rest 1
 	note A_, 1
-	note __, 1
+	rest 1
 	note G_, 2
-	note __, 2
-	note F#, 2
-	note __, 2
+	rest 2
+	note Fs, 2
+	rest 2
 	note E_, 2
-	note __, 2
+	rest 2
 	note B_, 2
-	note __, 2
+	rest 2
 	note A_, 12
-	note __, 2
-	note F#, 1
+	rest 2
+	note Fs, 1
 	note G_, 1
 	note A_, 1
 	note G_, 1
 	note A_, 1
 	note G_, 1
 	note A_, 1
-	note __, 1
+	rest 1
 	note G_, 1
-	note F#, 1
+	note Fs, 1
 	note E_, 1
-	note __, 5
-	endchannel
+	rest 5
 
-Music_Route101RSE_Ch3_branch_2:
+gbs_route101_Ch3_branch_2:
 	note A_, 1
-	note __, 1
+	rest 1
 	note A_, 1
-	note __, 1
+	rest 1
 	note A_, 1
-	note __, 1
+	rest 1
 	note A_, 1
 	note B_, 1
 	note A_, 6
-	note __, 2
-	endchannel
+	rest 2
 
-Music_Route101RSE_Ch4:
-	stereopanning $f
-	togglenoise $0
-	notetype $c
-	note __, 2
-Music_Route101RSE_Ch4_loop:
-	callchannel Music_Route101RSE_Ch4_branch_1
-	note D#, 1
-	note D#, 1
-	loopchannel 4, Music_Route101RSE_Ch4_loop
-Music_Route101RSE_Ch4_loop_2:
-	callchannel Music_Route101RSE_Ch4_branch_2
+gbs_route101_Ch4:
+	gbs_switch 3
+	stereo_panning FALSE, FALSE
+	toggle_noise 0
+	drum_speed 12
+	rest 2
+gbs_route101_Ch4_loop:
+	sound_call gbs_route101_Ch4_branch_1
+	note Ds, 1
+	note Ds, 1
+	sound_loop 4, gbs_route101_Ch4_loop
+gbs_route101_Ch4_loop_2:
+	sound_call gbs_route101_Ch4_branch_2
 	note D_, 1
 	note B_, 1
 	note B_, 1
 	note B_, 1
-	note D#, 1
+	note Ds, 1
 	note B_, 1
 	note D_, 1
 	note D_, 1
-	callchannel Music_Route101RSE_Ch4_branch_2
-	note D_, 1
-	note B_, 1
-	note B_, 1
-	note D_, 1
-	note D#, 1
-	note B_, 1
-	note D_, 1
-	note B_, 1
-	loopchannel 3, Music_Route101RSE_Ch4_loop_2
+	sound_call gbs_route101_Ch4_branch_2
 	note D_, 1
 	note B_, 1
 	note B_, 1
 	note D_, 1
-	note D#, 2
+	note Ds, 1
+	note B_, 1
+	note D_, 1
+	note B_, 1
+	sound_loop 3, gbs_route101_Ch4_loop_2
+	note D_, 1
+	note B_, 1
+	note B_, 1
+	note D_, 1
+	note Ds, 2
 	note F_, 1
 	note F_, 1
 	note F_, 2
-	note D#, 1
-	note D#, 1
+	note Ds, 1
+	note Ds, 1
 	note D_, 2
-	note F#, 2
+	note Fs, 2
 	note D_, 1
 	note B_, 1
 	note B_, 1
 	note D_, 1
-	note D#, 2
+	note Ds, 2
 	note F_, 1
 	note F_, 1
 	note F_, 2
-	note D#, 1
-	note D#, 1
+	note Ds, 1
+	note Ds, 1
 	note D_, 4
-	loopchannel 0, Music_Route101RSE_Ch4_loop
+	sound_loop 0, gbs_route101_Ch4_loop
 
-Music_Route101RSE_Ch4_branch_1:
-	note D#, 4
+gbs_route101_Ch4_branch_1:
+	note Ds, 4
 	note F_, 3
-	note D#, 1
-	note D#, 4
+	note Ds, 1
+	note Ds, 4
 	note F_, 2
-	note D#, 1
-	note D#, 1
-	note D#, 4
+	note Ds, 1
+	note Ds, 1
+	note Ds, 4
 	note F_, 4
-	note D#, 2
+	note Ds, 2
 	note F_, 1
 	note F_, 1
 	note F_, 2
-	endchannel
 
-Music_Route101RSE_Ch4_branch_2:
+gbs_route101_Ch4_branch_2:
 	note D_, 1
 	note B_, 1
 	note B_, 1
 	note B_, 1
-	note D#, 1
+	note Ds, 1
 	note B_, 1
 	note D_, 1
 	note B_, 1
-	endchannel
+
+	.align 4
+gbs_route101:
+	.byte 4	@ NumTrks
+	.byte 0	@ NumBlks
+	.byte 0	@ Priority
+	.byte 0	@ Reverb
+
+	.int voicegroup000
+
+	.int gbs_route101_Ch1
+	.int gbs_route101_Ch2
+	.int gbs_route101_Ch3
+	.int gbs_route101_Ch4
